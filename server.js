c2421e436712c89389701ab354d08d8b06b5b0b3
Harshal Patil <harshalpatil0007@users.noreply.github.com>
2026-03-05 08:11:52 +0530

    Day 2: Authentication APIs (Login & Register)

--- server.js
+++ server.js
@@ -3,16 +3,19 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 
+const authRoutes = require("./routes/authRoutes");
+
 dotenv.config();
 
 const app = express();
 
-// middleware
 app.use(cors());
 app.use(express.json());
 
-// database connection
 connectDB();
+
+// Routes
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.send("Project Management API Running");
